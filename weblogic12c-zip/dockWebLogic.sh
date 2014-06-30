e073b4c09a5c18e04cd138072fec1e781b373597
Bruno Borges <bruno.borges@gmail.com>
2014-06-30 16:36:03 -0300

    added name and host port attachment

--- weblogic12c-zip/dockWebLogic.sh
+++ weblogic12c-zip/dockWebLogic.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run oracle/weblogic12c_dev /u01/oracle/wls12130/user_projects/domains/base_domain/startWebLogic.sh
+docker rm wls12cdev
+docker run -d -p 7001:7001 --name wls12cdev oracle/weblogic12c_dev /u01/oracle/wls12130/user_projects/domains/base_domain/startWebLogic.sh
